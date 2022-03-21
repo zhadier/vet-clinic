295f54cf99da23ced8ebc8d1b81ffdd907e4d127
Zeeshan Haider <zhadier39@gmail.com>
2022-03-22 03:45:33 +0500

    add missing semi colon

--- queries.sql
+++ queries.sql
@@ -27,65 +27,8 @@ SELECT
 FROM
   animals
 WHERE
-  name IN ('Agumon', 'Pikachu')
-SELECT
-  name,
-  escape_attempts
-FROM
-  animals
-WHERE
-  weight_kg > 10.5;
+  name IN ('Agumon', 'Pikachu');
 
-SELECT
-  *
-from
-  animals
-WHERE
-  neutered = true;
-
-SELECT
-  *
-from
-  animals
-WHERE
-  name != 'Gabumon';
-
-SELECT
-  *
-from
-  animals
-WHERE
-  weight_kg BETWEEN 10.4
-  AND 17.3;/*Queries that provide answers to the questions from all projects.*/
-SELECT
-  *
-FROM
-  animals
-WHERE
-  name LIKE '%mon%';
-
-SELECT
-  name
-FROM
-  animals
-WHERE
-  date_of_birth BETWEEN '2016-01-01'
-  AND '2019-01-01';
-
-SELECT
-  name
-FROM
-  animals
-WHERE
-  neutered = true
-  AND escape_attempts < 3;
-
-SELECT
-  date_of_birth
-FROM
-  animals
-WHERE
-  name IN ('Agumon', 'Pikachu')
 SELECT
   name,
   escape_attempts
